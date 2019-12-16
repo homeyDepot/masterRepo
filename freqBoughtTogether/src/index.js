c3aed23fc6c2e74f714de6d88ca4f99183a991a9
Student Five <you@example.com>
2019-12-16 11:04:19 -0600

    add freq bought component

---
+++ freqBoughtTogether/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./main";
+import "./index.css";
+
+ReactDOM.render(
+    <Main/>, document.getElementById("root")
+);
