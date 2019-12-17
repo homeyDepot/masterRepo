9eaecfaba92c3fe2a6f9972c5143a08b7c5eceb8
Student Five <you@example.com>
2019-12-17 10:30:16 -0600

    updated

--- freqBoughtTogetherComp/src/home.js
+++ freqBoughtTogetherComp/src/home.js
@@ -4,7 +4,7 @@ class Home extends Component{
     render(){
       return(
             <div>
-              <h1 class="fbt" >Frequently Bought Together</h1>
+              <h1 className="fbt" >Frequently Bought Together</h1>
               
             
             </div>
--- freqBoughtTogetherComp/src/imagedisplay.js
+++ freqBoughtTogetherComp/src/imagedisplay.js
@@ -6,33 +6,33 @@ import './style.css'
 class Imagedisplay extends Component{
     render(){
       return(
-        <div id="row" class="fbtProduct-Line">
+        <div id="row" className="fbtProduct-Line">
 
                 <div>
-                  <img class="fbtProductImage" src="https://images.homedepot-static.com/productImages/aa5154d6-e176-463c-8225-cd16c0b89140/svn/chrome-moen-bathtub-shower-faucet-combos-82603-64_145.jpg"></img>
+                  <img className="fbtProductImage" src="https://images.homedepot-static.com/productImages/aa5154d6-e176-463c-8225-cd16c0b89140/svn/chrome-moen-bathtub-shower-faucet-combos-82603-64_145.jpg"></img>
                 </div>               
-                <p class="plus">+</p> 
+                <p className="plus">+</p> 
 
                 <div>  
-                  <img class="fbtProductImage" src="https://images.homedepot-static.com/productImages/012d00bf-b275-445b-84a7-d77dfe14af91/svn/chrome-moen-shower-faucets-82604-64_145.jpg"></img>
+                  <img className="fbtProductImage" src="https://images.homedepot-static.com/productImages/012d00bf-b275-445b-84a7-d77dfe14af91/svn/chrome-moen-shower-faucets-82604-64_145.jpg"></img>
                 </div> 
 
-                <p class="plus">+</p> 
+                <p className="plus">+</p> 
 
                 <div>
-                  <img class="fbtProductImage" src="https://images.homedepot-static.com/productImages/df8e9b7f-6013-48bc-a1b3-f99082482eff/svn/white-glacier-bay-two-piece-toilets-n2316-64_145.jpg"></img>
+                  <img className="fbtProductImage" src="https://images.homedepot-static.com/productImages/df8e9b7f-6013-48bc-a1b3-f99082482eff/svn/white-glacier-bay-two-piece-toilets-n2316-64_145.jpg"></img>
                 </div>
 
-                <div class="priceBlock">
-                    <span class="fbtAtcPrice">
+                <div className="priceBlock">
+                    <span className="fbtAtcPrice">
                         <span>Price For All Three:</span>
                         <br></br>
                         <br></br>
-                        <span class="fbtAtcPrice__currency">$</span>
-                        <span class="fbtAtcPrice__dollar">190</span>
-                        <span class="fbtAtcPrice__cents">00</span>
+                        <span className="fbtAtcPrice__currency">$</span>
+                        <span className="fbtAtcPrice__dollar">190</span>
+                        <span className="fbtAtcPrice__cents">00</span>
                         <br></br><br></br>
-                        <button onClick='' class="button">Add All Three To The Cart</button>
+                        <button className="button">Add All Three To The Cart</button>
                     </span>    
             </div>
             </div>
